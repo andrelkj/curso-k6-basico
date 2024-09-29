966ddeead2abd3bfc647afa0ff920ca517cd1a36
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-09-29 01:13:08 -0300

    add request duration limit and threshold

--- tests/signup.js
+++ tests/signup.js
@@ -1,11 +1,15 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-import uuid from './libs/uuid.js'
+import uuid from './libs/uuid.js';
 
 export const options = {
   vus: 10,
   duration: '30s',
+  thresholds: {
+    http_req_duration: ['p(95)<2000'], // 95% of requests are expected to run in less than 2 seconds
+    http_req_failed: ['rate<0.01'] // 1% of requests send can fail
+  },
 };
 
 export default function () {
